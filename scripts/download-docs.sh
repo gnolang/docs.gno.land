2613c6d363b83e7ce38e8b0de352e67a4152f07e
albttx <contact@albttx.tech>
2024-01-29 10:25:56 +0000

    fix: download-docs script

--- scripts/download-docs.sh
+++ scripts/download-docs.sh
@@ -5,4 +5,6 @@ ROOT_DIR="$(dirname "$0")/.."
 wget -O /tmp/gno.zip "https://github.com/gnolang/gno/archive/refs/heads/master.zip"
 unzip -d /tmp/ /tmp/gno.zip
 
-cp -r /tmp/gno-master/docs ./docs
+cp -r /tmp/gno-master/docs ${ROOT_DIR}/docs
+
+pwd ; ls -la ; ls -la $ROOT_DIR; ls -la $ROOT_DIR/docusaurus
