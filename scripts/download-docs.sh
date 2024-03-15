30920443a37533f64f906ac9afbf8a148af0d12c
Albert Le Batteux <contact@albttx.tech>
2024-03-15 15:30:49 +0000

    ci: add netlify deploy workflow (#3)
    * chore: deploy without comminting docs directory
    
    * fix: download-docs script
    
    * chore: remove debug
    
    * chore(ci): github action netlify
    
    * chore(ci): netlify cli deploy
    
    * chore: netlify update workflow
    
    * chore(ci): build website
    
    * fix: add working-dir
    
    * chore: update for gnolang/docs.gno.land repo
    
    * chore: change branch deploy

---
+++ scripts/download-docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ROOT_DIR="$(dirname "$0")/.."
+
+wget -O /tmp/gno.zip "https://github.com/gnolang/gno/archive/refs/heads/master.zip"
+unzip -d /tmp/ /tmp/gno.zip
+
+cp -r /tmp/gno-master/docs ${ROOT_DIR}/docs
