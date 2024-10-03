4b2161f21faf15ff5f95525ee4bae270271874da
leohhhn <hudakleon@gmail.com>
2024-10-03 19:44:15 +0200

    initial

--- scripts/download-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ROOT_DIR="$(dirname "$0")/.."
-
-wget -O /tmp/gno.zip "https://github.com/gnolang/gno/archive/refs/heads/master.zip"
-unzip -d /tmp/ /tmp/gno.zip
-
-cp -r /tmp/gno-master/docs ${ROOT_DIR}/docs
