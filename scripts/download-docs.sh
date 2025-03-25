7b7dab7a2cbc7286d2d39a0a205c9c436e8568c3
leohhhn <hudakleon@gmail.com>
2025-03-25 16:14:36 +0100

    temp

--- scripts/download-docs.sh
+++ scripts/download-docs.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+set -e  # Exit on error
+
 ROOT_DIR="$(dirname "$0")/.."
+TMP_DIR=$(mktemp -d)
 
-wget -O /tmp/gno.zip "https://github.com/gnolang/gno/archive/refs/heads/master.zip"
-unzip -d /tmp/ /tmp/gno.zip
+# Download and extract the repository
+wget -O "$TMP_DIR/gno.zip" "https://github.com/gnolang/gno/archive/refs/heads/master.zip"
+unzip "$TMP_DIR/gno.zip" -d "$TMP_DIR"
 
-cp -r /tmp/gno-master/docs ${ROOT_DIR}/docs
+# Remove existing docs/ folder if it exists
+rm -rf "$ROOT_DIR/docs"
+
+# Move only the docs/ folder
+mv "$TMP_DIR/gno-master/docs" "$ROOT_DIR/"
+
+# Cleanup
+rm -rf "$TMP_DIR"
